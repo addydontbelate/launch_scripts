a480c1f2a5999d6d53ab25f95bba943a9b2d1e60
Pranshu Malik <pranshumalik14@gmail.com>
2020-01-27 10:40:35 -0600

    made changes to run in proper order

--- addydontbelate_explore.sh
+++ addydontbelate_explore.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ~/catkin_ws
-souce /devel/setup.bash
-xterm -hold -e "roslaunch mie443_contest1 turtlebot_world.launch world:=1" &
-xterm -hold -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
-xterm -hold -e "rosrun mie443_contest1 contest1" &
-xterm -hold -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"
+source ~/catkin_ws/devel/setup.bash
+gnome-terminal -e "roslaunch mie443_contest1 turtlebot_world.launch world:=1" &
+gnome-terminal -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+gnome-terminal -e "rosrun mie443_contest1 contest1" &
+gnome-terminal -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &s
 
 exit 0
