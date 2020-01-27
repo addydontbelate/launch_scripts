c812723a810bbf1b8db7cae4966f73d21286cbf9
Pranshu Malik <pranshumalik14@gmail.com>
2020-01-27 10:42:24 -0600

    reomving extra chars

--- addydontbelate_explore.sh
+++ addydontbelate_explore.sh
@@ -5,6 +5,6 @@ source ~/catkin_ws/devel/setup.bash
 gnome-terminal -e "roslaunch mie443_contest1 turtlebot_world.launch world:=1" &
 gnome-terminal -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
 gnome-terminal -e "rosrun mie443_contest1 contest1" &
-gnome-terminal -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &s
+gnome-terminal -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"
 
 exit 0
